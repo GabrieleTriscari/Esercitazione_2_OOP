c70700b43e50fcaf3a055e507ffb959f1f1f30f3
GabrieleTriscari <s312175@studenti.polito.it>
2025-04-01 16:26:46 +0200

    update

--- complex.cpp
+++ complex.cpp
@@ -8,6 +8,7 @@ int main() {
     complex_number<float> seconda_copia_c1 = c1;
     complex_number<float> terza_copia_c1 = c1;
     complex_number<float> c2(6.0, -9.0);
+    complex_number<float> prima_copia_c2 = c2;
 
     std::cout << "Numero complesso 1: " << c1 << std::endl;
     std::cout << "Numero complesso 2: " << c2 << std::endl;
@@ -32,73 +33,83 @@ int main() {
 
     prima_copia_c1 *= c2;
     std::cout << "*= prima copia con c2:  " << prima_copia_c1 << std::endl; //errore di calcolo
-
-    
-    
     
     seconda_copia_c1 += 3.0; 
-    std::cout << "c1 dopo aver sommato 3.0: " << seconda_copia_c1 << std::endl;
+    std::cout << "c1 += 3.0: " << seconda_copia_c1 << std::endl;
 
-    complex_number<float> c2new = c2 * 2.0f;  
-    std::cout << "c2 dopo aver molt. per  2.0: " << c2new << std::endl;
-    std::cout << "commutatività del prodotto per scalare:" << 2.0f * c2 << std::endl;
+    complex_number<float> sum2 =  terza_copia_c1+3.0f ; 
+    std::cout << "c1 + 3.0: " << sum2 << std::endl;
+    std::cout << "commutatività della somma: " <<  3.0f + terza_copia_c1<< std::endl;
+
+    c2 *= 2.0; 
+    std::cout << "c1 *= 2.0: " << c2 << std::endl;
+
+    complex_number<float> c2new = prima_copia_c2 * 2.0f;  
+    std::cout << "c2 * 2.0: " << c2new << std::endl;
+    std::cout << "commutatività del prodotto:" << 2.0f * prima_copia_c2 << std::endl;
 
     
-    complex_number<float> sum2 = 3.0f + terza_copia_c1; 
-    std::cout << "Somma tra un scalare 3.0 e il numero complesso c1: " << sum2 << std::endl;
-    std::cout << "commutatività" << terza_copia_c1 + 3.0 << std::endl;
+    
     complex_number<float> prod2 = 2.0f * terza_copia_c1 ; 
     std::cout << "Prodotto tra un scalare 2.0 e il numero complesso c1: " << prod2 << std::endl;
     std::cout << "commutatività" << terza_copia_c1 * 2.0 << std::endl;
-    std::cout << "Se in input ho dei double: " << std::endl;
+   
+   
 
-    
-   /* complex_number<double> c3(3.0, 5.0);
-    complex_number<double> c4(6.0, -9.0);
+    complex_number<double> c3(3.0, 5.0);
     complex_number<double> prima_copia_c3 = c3;
     complex_number<double> seconda_copia_c3 = c3;
     complex_number<double> terza_copia_c3 = c3;
-
+    complex_number<double> c4(6.0, -9.0);
+    complex_number<double> prima_copia_c4 = c4;
 
     std::cout << "Numero complesso 1: " << c3 << std::endl;
     std::cout << "Numero complesso 2: " << c4 << std::endl;
 
-    
-    complex_number<double> somma = c3 + c4;
-    std::cout << "Somma: " << somma << std::endl;
+    std::cout << "Parte reale di c1: " << c3.real() << std::endl;
+    std::cout << "Parte immaginaria di c1: " << c3.imag() << std::endl;
 
-    
+    std::cout << "Coniugato di c1: " << c3.coniugato() << std::endl;
+
+
+
+    complex_number<double> sum_d = c3 + c4;
+    std::cout << "+ c1 con c2: " << sum_d << std::endl;
+
+
     c3 += c4;
-    std::cout << "Somma tramite +=: stampo il nuovo c1 " << c3 << std::endl;
+    std::cout << "+= c1 con c2: " << c3 << std::endl; 
+
+
+    complex_number<double> prod_d = prima_copia_c3 * c4;
+    std::cout << "* prima copia c1 con c2: " << prod_d << std::endl;
 
     prima_copia_c3 *= c4;
-    std::cout << "prodotto tramite *=: stampo il nuovo c1  " << prima_copia_c3<< std::endl;
+    std::cout << "*= prima copia con c2:  " << prima_copia_c3 << std::endl; //errore di calcolo
 
-    
-    complex_number<double> prodotto = seconda_copia_c3 * c4;
-    std::cout << "Prodotto: " << prodotto << std::endl;
-
-    
-    std::cout << "Coniugato di c2: " << c4.coniugato() << std::endl;
-
-    
     seconda_copia_c3 += 3.0; 
-    std::cout << "c1 dopo aver sommato 3.0: " << seconda_copia_c3 << std::endl;
+    std::cout << "c1 += 3.0: " << seconda_copia_c3 << std::endl;
 
-    c4 = c4 * 2.0;  
-    std::cout << "c2 dopo aver molt. per  2.0: " << c4 << std::endl;
+    complex_number<double> sum2_d =  terza_copia_c3+3.0 ; 
+    std::cout << "c1 + 3.0: " << sum2_d << std::endl;
+    std::cout << "commutatività della somma: " <<  3.0 + terza_copia_c3<< std::endl;
 
+    c4 *= 2.0; 
+    std::cout << "c1 *= 2.0: " << c4 << std::endl;
+
+    complex_number<double> c4new = prima_copia_c4 * 2.0;  
+    std::cout << "c2 * 2.0: " << c4new << std::endl;
+    std::cout << "commutatività del prodotto:" << 2.0 * prima_copia_c4 << std::endl;
+
+
+
+    complex_number<double> prod2_d = 2.0 * terza_copia_c3 ; 
+    std::cout << "Prodotto tra un scalare 2.0 e il numero complesso c1: " << prod2_d << std::endl;
+    std::cout << "commutatività" << terza_copia_c3 * 2.0 << std::endl;
+
+        
     
-    complex_number<double> somma2 = 3.0 + terza_copia_c3; 
-    std::cout << "Somma tra un scalare 3.0 e il numero complesso c1: " << somma2 << std::endl;
-
-    complex_number<double> prodotto2 = 2.0 * terza_copia_c3; 
-    std::cout << "Prodotto tra un scalare 2.0 e il numero complesso c1: " << prodotto2 << std::endl;
-     
-
-
     
-*/
 
     return 0;
 
--- complex.hpp
+++ complex.hpp
@@ -37,10 +37,10 @@ public:
         return risultato;   
     }
 
-    complex_number& operator*=(const complex_number& other) {
+    complex_number<T> operator*=(const complex_number& other) {
         T temp = parte_reale;
         parte_reale = (parte_reale * other.parte_reale) - (parte_immaginaria * other.parte_immaginaria);
-        parte_immaginaria = (parte_reale * other.parte_immaginaria) +  (parte_immaginaria * other.parte_reale);
+        parte_immaginaria = (temp * other.parte_immaginaria) +  (parte_immaginaria * other.parte_reale);
         return *this;    
     }
 
@@ -93,72 +93,5 @@ complex_number<T> operator*(T value, const complex_number<T>& numero) {
 
 
 
-/*
-template <typename T>
-class complex_number {
-    static_assert(std::is_floating_point<T>::value, "T must be a floating-point type");
 
-private:
-    T parte_reale;
-    T parte_immaginaria;
 
-public:
-    complex_number() : parte_reale(T(0)), parte_immaginaria(T(0)) {}
-    complex_number(T re, T im) : parte_reale(re), parte_immaginaria(im) {}
-
-    T real() const {
-        return parte_reale;
-    }
-
-    T imag() const {
-        return parte_immaginaria;
-    }
-
-    complex_number<T> coniugato() const {
-        return complex_number<T>(parte_reale, -parte_immaginaria);
-    }
-
-    complex_number<T>& operator+=(const complex_number<T>& other) {
-        parte_reale += other.parte_reale;
-        parte_immaginaria += other.parte_immaginaria;
-        return *this;
-    }
-
-    complex_number<T> operator+(const complex_number<T>& other) const {
-        return complex_number<T>(parte_reale + other.parte_reale, parte_immaginaria + other.parte_immaginaria);
-    }
-
-    complex_number<T>& operator*=(const complex_number<T>& other) {
-        parte_reale = parte_reale * other.parte_reale - parte_immaginaria * other.parte_immaginaria;
-        parte_immaginaria = parte_reale * other.parte_immaginaria + parte_immaginaria * other.parte_reale;
-    
-        return *this;
-    }
-
-    complex_number<T> operator*(const complex_number<T>& other) const {
-        return complex_number<T>(
-            parte_reale * other.parte_reale - parte_immaginaria * other.parte_immaginaria,
-            parte_reale * other.parte_immaginaria + parte_immaginaria * other.parte_reale
-        );
-    }
-
-    complex_number<T> operator+(T value){
-        return complex_number<T>(parte_reale + value, parte_immaginaria);
-    }
-
-    complex_number<T> operator+=(T value){
-        parte_reale += value;
-        return *this;
-    }
-
-    complex_number<T>& operator*=(T value) {
-        parte_reale *= value;
-        parte_immaginaria *= value;
-        return *this;
-    }
-
-    complex_number<T> operator*(T value){
-        return complex_number<T>(parte_reale * value, parte_immaginaria * value);
-    }
-}
-*/
